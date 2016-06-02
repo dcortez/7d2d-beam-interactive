5624908bb0628112861bda4df133b682c5a67fd0
Toby Cortez <Tcortez@fullsail.edu>
2016-06-02 15:46:16 -0700

    It all works as is

--- interactive.py
+++ interactive.py
@@ -1,10 +1,6 @@
-import asyncio
-import requests
-import os
+import asyncio, requests, os, json, random
 from beam_interactive import start
 from beam_interactive import proto
-from random import random
-import json
 
 #Beam Interactive Login
 path = "https://beam.pro/api/v1"
@@ -15,10 +11,14 @@ Player = {
     "password": "PASSWORD"
 }
 
+PYTHON_PATH = '<PATH_TO_PYTHON>'; // Path to Python.. can be python2.7 or above for this entry
+PYSCRIPT_PATH = '<PATH_TO_TELNET.PY>'; //Path to Python Telnet Script - USE "/" as seperator even on Windows
+
 #7 Days to Die Player Name
 GAME_PLAYERID = '<7Days_Playername>'
 
 #Spawn Lists
+#items = [ITEM, QUANTITY]
 items = [
 	['gunPistol', 1],
 	['gunPumpShotgun', 1],
@@ -55,6 +55,8 @@ friends = [
 	'animalStag',
 	'animalPig'
 ]
+#Random range of numbers
+num = random.randrange(0, 100)
 
 ###################################################################################
 ###########################   NO EDIT BELOW THIS LINE   ###########################
@@ -71,90 +73,114 @@ class AuthenticationException(Exception):
 
 class Beam():
 
-    def get_tetris(self, session, channel):
-        """Retrieve interactive connection information."""
-        return session.get(path + "/tetris/{id}/robot".format(id=channel)).json()
+	def get_tetris(self, session, channel):
+		"""Retrieve interactive connection information."""
+		return session.get(path + "/tetris/{id}/robot".format(id=channel)).json()
 
-    def go_interactive(self, session, channel_id, version, code):
-        interactiveDetails = dict(tetrisGameId=version, tetrisShareCode=code, interactive=1)
-        return session.put(path + "/channels/{id}".format(id=channel_id), interactiveDetails).json()
+	def go_interactive(self, session, channel_id, version, code):
+		interactiveDetails = dict(tetrisGameId=version, tetrisShareCode=code, interactive=1)
+		return session.put(path + "/channels/{id}".format(id=channel_id), interactiveDetails).json()
 
-    def on_handshake(self, line, conn):
-        print("Shaking hands\n")
+	def on_handshake(self, line, conn):
+		print("Shaking hands\n")
 
-    def on_handshake_ack(self, line, conn, player):
-        print("Shaken hands\n")
+	def on_handshake_ack(self, line, conn, player):
+		print("Shaken hands\n")
 
-    def on_error(self, error, conn):
-        """This is called when we get an Error packet. It contains
-        a single attribute, 'message'.
-        """
-        print('Oh no, there was an error!\n')
-        print(error.message)
+	def on_error(self, error, conn):
+		"""This is called when we get an Error packet. It contains
+		a single attribute, 'message'.
+		"""
+		print('Oh no, there was an error!\n')
+		print(error.message)
 
-    def login(self, session, username, password):
-        """Log into the Beam servers via the API."""
-        user = dict(username=username, password=password)
-        return session.post(path + "/users/login", user).json()
+	def login(self, session, username, password):
+		"""Log into the Beam servers via the API."""
+		user = dict(username=username, password=password)
+		return session.post(path + "/users/login", user).json()
 
-    def on_report(self, report, conn, player):
-        
-        #print(report)
+
+		
+	def on_report(self, report, conn, player):
+
         #Example of how to parse the reports
-        for tactile in report.tactile:
-			
-            #print(tactile)
-            if tactile.pressFrequency > 0 and tactile.id == 0:
-                print('SPAWN RANDOM ANIMAL\n')
-                os.system('python /vhosts/interactive/r2d2/telnet.py 3 spawnentity AtomicYetiGaming animalChicken')
-            elif tactile.pressFrequency > 0 and tactile.id == 1:
-                print('SPAWN RANDOM ENEMY\n')
-            elif tactile.pressFrequency > 0 and tactile.id == 2:
-                print('SPAWN RANDOM ITEM\n')
-            elif tactile.pressFrequency > 0 and tactile.id == 3:
-                print('SPAWN HORDE\n')
-            elif tactile.pressFrequency > 0 and tactile.id == 4:
-                print('SPAWN FERAL\n')
-            elif tactile.pressFrequency > 0 and tactile.id == 5:
-                print('SPAWN SCREAMER\n')
-            elif tactile.pressFrequency > 0 and tactile.id == 6:
-                print('SPAWN AIRDROP\n')
+		for tactile in report.tactile:
+
+            #Spawn Animal
+			if tactile.pressFrequency > 0 and tactile.id == 0:
+				KEY = random.randrange(0, len(friends))
+				os.system(('python {} 3 spawnentity {} {}').format(PYSCRIPT_PATH, GAME_PLAYERID, friends[KEY]))
+            
+            #Spawn Enemy
+			elif tactile.pressFrequency > 0 and tactile.id == 1:
+				KEY = random.randrange(0, len(enemies))
+				if num == 73:
+					os.system(('python {} 2 spawnwanderinghorde').format(PYSCRIPT_PATH))
+				else:
+					os.system(('python {} 2 spawnentity {} {}').format(PYSCRIPT_PATH, GAME_PLAYERID, enemies[KEY]))
+					
+            #Spawn Item    
+			elif tactile.pressFrequency > 0 and tactile.id == 2:
+				KEY = random.randrange(0, len(items))
+				if num == 73:
+					os.system(('python {} 1 spawnairdrop').format(PYSCRIPT_PATH))
+				else:
+					os.system(('python {} 4 spawnentity {} {} {}').format(PYSCRIPT_PATH, GAME_PLAYERID, items[KEY][0], items[KEY][1]))
+										
+			#Spawn Horde
+			elif tactile.pressFrequency > 0 and tactile.id == 3:
+				KEY = random.randrange(0, len(enemies))
+				os.system(('python {} 2 spawnwanderinghorde').format(PYSCRIPT_PATH))
+
+			#Spawn Feral
+			elif tactile.pressFrequency > 0 and tactile.id == 4:
+				KEY = random.randrange(0, len(enemies))
+				os.system(('python {} 2 spawnentity {} zombieFeral').format(PYSCRIPT_PATH, GAME_PLAYERID))
+
+			elif tactile.pressFrequency > 0 and tactile.id == 5:
+				KEY = random.randrange(0, len(enemies))
+				os.system(('python {} 2 spawnentity {} zombieScreamer').format(PYSCRIPT_PATH, GAME_PLAYERID))
+
+			#Spawn Airdrop
+			elif tactile.pressFrequency > 0 and tactile.id == 6:
+				KEY = random.randrange(0, len(enemies))
+				os.system(('python {} 1 spawnairdrop').format(PYSCRIPT_PATH))
             
 
-    @asyncio.coroutine
-    def connect(self, user, player):
-        session = requests.Session()
-        channel_data = Beam().login(session, **user)
+	@asyncio.coroutine
+	def connect(self, user, player):
+		session = requests.Session()
+		channel_data = Beam().login(session, **user)
 
-        if not "channel" in channel_data:
-            raise AuthenticationException("Incorrect username or password\n")
+		if not "channel" in channel_data:
+			raise AuthenticationException("Incorrect username or password\n")
 
-        channel_id = channel_data["channel"]["id"]
-        data = Beam().get_tetris(session, channel_id)
-        interactiveData = Beam().go_interactive(session, channel_id, game_version, share_code)
-        conn = yield from start(data['address'], channel_id, data['key'], loop)
-        print("Using channel ID: ", channel_id, "\n")
+		channel_id = channel_data["channel"]["id"]
+		data = Beam().get_tetris(session, channel_id)
+		interactiveData = Beam().go_interactive(session, channel_id, game_version, share_code)
+		conn = yield from start(data['address'], channel_id, data['key'], loop)
+		print("Using channel ID: ", channel_id, "\n")
 
         # Handlers definitions
-        handlers = {
-            proto.id.error: Beam().on_error,
-            proto.id.report: Beam().on_report,
-            proto.id.handshake: Beam().on_handshake,
-            proto.id.handshake_ack: Beam().on_handshake_ack
-        }
+		handlers = {
+			proto.id.error: Beam().on_error,
+			proto.id.report: Beam().on_report,
+			proto.id.handshake: Beam().on_handshake,
+			proto.id.handshake_ack: Beam().on_handshake_ack
+		}
 
         # Actual parsing of the recieved packet
-        while (yield from conn.wait_message()):
-            decoded, packet_bytes = conn.get_packet()
-            packet_id = proto.id.get_packet_id(decoded)
-            if decoded is None:
-                    print('We got a bunch of unknown bytes.')
-            elif packet_id in handlers:
-                    handlers[packet_id](decoded, conn, player)
-            else:
-                    print("We got packet {} but didn't handle it!".format(packet_id))
-        print("Closing\n")
-        conn.close()
+		while (yield from conn.wait_message()):
+			decoded, packet_bytes = conn.get_packet()
+			packet_id = proto.id.get_packet_id(decoded)
+			if decoded is None:
+				print('We got a bunch of unknown bytes.')
+			elif packet_id in handlers:
+				handlers[packet_id](decoded, conn, player)
+			else:
+				print("We got packet {} but didn't handle it!".format(packet_id))
+				print("Closing\n")
+				conn.close()
 
 # Create another object like this to add another player
 player = Beam()
@@ -167,4 +193,3 @@ except KeyboardInterrupt:
     print("Disconnected.")
 finally:
     loop.close()
-    
